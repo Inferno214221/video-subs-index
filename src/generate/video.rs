c180d9f124d52f53e7b872aad3c06d2a45823521
Inferno214221 <inferno214221@gmail.com>
2026-06-12 12:25:59 +0930

    Significantly improves generate video speed and quality.

--- src/generate/video.rs
+++ src/generate/video.rs
@@ -1,6 +1,6 @@
-use std::{path::Path, range::RangeInclusive, time::Duration};
+use std::{path::Path, process::Command, range::RangeInclusive, time::Duration};
 
-use ffmpeg_light::{Time, TranscodeBuilder};
+use ffmpeg_light::Time;
 use srt_subtitles_parser::{Subtitle, Timestamp};
 
 pub fn convert_timestamp_with_offset(ts: &Timestamp, millis: i64) -> Time {
@@ -19,14 +19,34 @@ pub fn slice_video_range(
     sub_range: RangeInclusive<&Subtitle>
 ) {
     let RangeInclusive { start: first, last } = sub_range;
-    TranscodeBuilder::new()
-        .input(source)
-        .output(target)
-        .overwrite(true)
-        .extra_arg("-ss")
-        .extra_arg(format!("{}", convert_timestamp_with_offset(&first.start, 0)))
-        .extra_arg("-to")
-        .extra_arg(format!("{}", convert_timestamp_with_offset(&last.end, -0)))
-        .run()
-        .unwrap();
+    // -ss and -to before -i slices the input before decoding. This is a requirement when applying
+    // palette generation, so that we only get the colors we need. It also offers a HUGE speedup.
+    Command::new("ffmpeg")
+        // Input start time
+        .arg("-ss").arg(convert_timestamp_with_offset(&first.start, 0).to_string())
+        // Input end time
+        .arg("-to").arg(convert_timestamp_with_offset(&last.end, 0).to_string())
+        // Set input source
+        .arg("-i").arg(source.as_ref())
+        // Generate and use palette
+        .arg("-vf").arg("split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse")
+        // Output to target
+        .arg(target.as_ref())
+        // Overwrite existing files
+        .arg("-y")
+        .spawn().expect("failed to spawn ffmpeg")
+        .wait().expect("failed to wait for ffmpeg to finish")
+        .exit_ok().expect("ffmepg task failed");
+
+    // extra_arg are all appended at the end, which won't work for input slicing
+    // TranscodeBuilder::new()
+    //     .extra_arg("-ss")
+    //     .extra_arg(format!("{}", convert_timestamp_with_offset(&first.start, 0)))
+    //     .extra_arg("-to")
+    //     .extra_arg(format!("{}", convert_timestamp_with_offset(&last.end, -0)))
+    //     .input(source)
+    //     .output(target)
+    //     .overwrite(true)
+    //     .run()
+    //     .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(iter_map_windows)]
 #![feature(iterator_try_collect)]
+#![feature(exit_status_error)]
 
 #[macro_use] extern crate rocket;
 
